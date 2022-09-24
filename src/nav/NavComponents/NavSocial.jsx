06b8c0cdcb1f2ed2d83040a245221081fa4bab0e
intern-desktop <justin.c.kirk@gmail.com>
2022-09-24 11:18:50 -0400

    fixes social links

--- src/nav/NavComponents/NavSocial.jsx
+++ src/nav/NavComponents/NavSocial.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {FaGithub, FaYoutube, FaInstagram} from 'react-icons/fa';
+import {FaGithub, FaLinkedin, FaInstagram} from 'react-icons/fa';
 
 const NavSocial = () => {
   return (
     <div className='NavSocial'>
-      <a href='#' target='_blank'>
+      <a href='https://github.com/intern-jck' target='_blank'>
         <FaGithub size={60}/>
       </a>
-      <a href='#' target='_blank'>
-        <FaYoutube size={60}/>
+      <a href='https://www.linkedin.com/in/justinckirk/' target='_blank'>
+        <FaLinkedin size={60}/>
       </a>
-      <a href='#' target='_blank'>
+      <a href='https://www.instagram.com/makerinchief/' target='_blank'>
         <FaInstagram size={60}/>
       </a>
     </div>
